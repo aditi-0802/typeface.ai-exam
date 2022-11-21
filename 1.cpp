e9c8945b7dc68ff3abdb28a435bb002af4e6c90d
aditi-0802 <61201436+aditi-0802@users.noreply.github.com>
2022-11-21 16:02:09 +0530

    Create 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+void convertToBase(int x)
+{
+    if(x==0)
+    {
+        return;
+    }
+    int d=x%3;
+    x=x/3;
+    if(d<0)
+    {
+        x+=1;
+    }
+    convertToBase(x);
+    if(d<0)
+    {
+        cout<<"d+(3*-1)";
+    }
+    else{
+        cout<<d;
+    }
+
+
+}
+int main()
+{
+    int n;
+    cout<<"enter num"<<endl;
+    cin>>n;
+
+    if(n==0)
+    {
+        cout<<"0"<<endl;
+    }
+    else
+    {
+        convertToBase(n);
+    }
+    return 0;
+}
